9a57e8f87f4a47c51e54f27b71fbfc6b39d150b9
Ashok <ashokthapa2052@gmail.com>
2019-12-10 02:20:41 +0545

    adding project

---
+++ WebAppDemo/WebAppDemo/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppDemo.Models;
+using System.Web.Security;
+
+namespace WebAppDemo.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public  ActionResult LogIn(Models.Membership model)
+        {
+            using (var context =new OfficeDbEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid username and password");
+                return View();
+            }
+            
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using(var context = new OfficeDbEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("LogIn");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogIn");
+        }
+    }
+}
---
+++ WebAppDemo/WebAppDemo/Models/Membership.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppDemo.Models
+{
+    public class Membership
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
